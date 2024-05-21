afbe33b4c5dfbabcfb698b841ab8d55d2dec2474
Palak Bansal <palakbansal3233@gmail.com>
2024-05-21 12:24:13 +0530

    Recent Projects Section

--- data/index.ts
+++ data/index.ts
@@ -1,7 +1,6 @@
 export const navItems = [
   { name: "About", link: "#about" },
   { name: "Projects", link: "#projects" },
-  { name: "Testimonials", link: "#testimonials" },
   { name: "Contact", link: "#contact" },
 ];
 
@@ -9,7 +8,7 @@ export const gridItems = [
   {
     id: 1,
     title:
-      "My key focus is on open collaboration to ensure coherence between everyone",
+      "Words I live by: If you can't make it good, atleast make it look good.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
@@ -73,35 +72,35 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
-    img: "/p1.svg",
+    title: "Mutual Funds Explorer",
+    des: "Explore the world of mutual funds that can help you achieve financial goals and create wealth",
+    img: "/mf.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
+    link: "https://www.dezerv.in/mutual-funds/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
+    title: "HSN Code Finder",
+    des: "Find HSN Code or ITC HS Code and their GST Rates for your product with this HSN Code Search Tool",
+    img: "/hsn.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
+    link: "https://www.dripcapital.com/hsn-code/",
   },
   {
     id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
+    title: "OpenAI Article Summarizer",
+    des: "Revolutionize Your Reading: Discover an Open-Source Article Summarizer That Condenses Lengthy Articles into Digestible Gems of Knowledge!",
+    img: "/ai.png",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
+    link: "https://summarize-tau.vercel.app/",
   },
   {
     id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
+    title: "Testing in React - Deep Dive",
+    des: "What to test? What NOT to test? Tools for Static Analysis Testing",
+    img: "/test.png",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    link: "https://github.com/palakbansal3233/react-testing",
   },
 ];
 
