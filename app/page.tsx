7df4a90f6b6466fd08cce7184dcb218b2d12deb3
Palak Bansal <palakbansal3233@gmail.com>
2024-05-23 16:42:19 +0530

    Footer

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Grid from "@/components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Footer />
       </div>
     </main>
   );
--- data/index.ts
+++ data/index.ts
@@ -173,13 +173,11 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
-  },
-  {
-    id: 2,
-    img: "/twit.svg",
+    link: "https://github.com/palakbansal3233",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/palak-b-73278114a/",
   },
 ];
